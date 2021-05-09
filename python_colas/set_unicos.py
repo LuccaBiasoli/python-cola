c7ce5c618e2ae8a380b27ef865163adcfbc4f2f4
Lucca Biasoli <lucca.bbiasoli@gmail.com>
2021-05-09 03:08:51 -0300

    Create set_unicos.py

---
+++ python_colas/set_unicos.py
@@ -0,0 +1,10 @@
+lista = [1 ,1 ,1 ,4 ,78 ,78 ,4,'foda','mcdonalds','foda','mcdonalds']
+print(lista)
+print(set(lista))
+#///
+#set transforma tudo dentro unico
+vor = set('Mississipi')
+print(vor)
+#///
+print(set('3 , 4 ,3 ,4'))
+print(f'sabia q ia dar isso {set(lista)}')
