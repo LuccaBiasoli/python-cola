cb9d232b476be53f03fac8d93d08bb8ad7db5dec
Lucca Biasoli <lucca.bbiasoli@gmail.com>
2021-05-10 03:41:30 -0300

    0

---
+++ python_colas/forLoop.py
@@ -0,0 +1,8 @@
+#lista = [1, 2, 3 , 4 ,5 , 6, 7, 8, 9 ,10]
+#listao = ['um','dois','tres','quatro','cinco','seis','sete','oito','nove','dez']
+#contador = (lista[0]) , (listao[0])
+#for num in lista and listao:
+    #if lista and listao :
+        #print(f'Numero = {lista} ,escrito = {listao}')
+        #contador += 1
+
---
+++ python_colas/forLoopIn.py
@@ -0,0 +1,4 @@
+num = [(1,'um'), (2,'dois'), (3,'tres'), (4,'4')]
+
+for lets in num:
+    print(lets)
