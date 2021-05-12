4d2cf81743e8cc1ec3873366f80e005096e5819f
Lucca Biasoli <lucca.bbiasoli@gmail.com>
2021-05-12 05:50:36 -0300

    21

---
+++ python_colas/def1,.py
@@ -0,0 +1,20 @@
+def soma (num1,num2):
+    return num1 + num2 
+    
+
+somado = soma(1,2)
+print(somado)
+
+# # # 
+
+def soma (num1,num2):
+    print(num1 + num2)
+    
+soma(1,2)
+# # 3
+
+def soma (num1 = 'nao', num2 = 'sei'):
+    return num1 + num2 
+
+somadao  = soma()
+print(somadao)
