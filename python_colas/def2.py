e60ff9c7a00abb546442181807284aaf6e754883
Lucca Biasoli <lucca.bbiasoli@gmail.com>
2021-05-13 04:32:43 -0300

    Update def2.py

--- python_colas/def2.py
+++ python_colas/def2.py
@@ -1,5 +1,41 @@
-listao = [('sim', 2) , ('nao', 1), ('talvez', 3) , ('nunca', 4)]
+funcmes = [('vitor', 100), ('rafael', 320) , ('lucca', 400)]
 
 
-for number, numero in listao:
-    print('o resultado final deu :' ,  numero+(2 * numero))
+def funcionario_mes (funcmes):
+    atual_max = 0
+    func_do_mes = ''
+
+    for funcs, horas in (funcmes):
+        if horas > atual_max:
+            atual_max = horas
+            func_do_mes = funcs 
+        else:
+            pass
+
+    return (func_do_mes, atual_max)
+
+res_func = funcionario_mes(funcmes)
+print(res_func)
+
+
+################# 
+# MESMO EXEMPLO QUE EM CIMA SO QUE SALVANDO SEPARADAMENTE NO FINAL 
+
+funcmes = [('vitor', 100), ('rafael', 320) , ('lucca', 400)]
+
+
+def funcionario_mes (funcmes):
+    atual_max = 0
+    func_do_mes = ''
+
+    for funcs, horas in (funcmes):
+        if horas > atual_max:
+            atual_max = horas
+            func_do_mes = funcs 
+        else:
+            pass
+
+    return (func_do_mes, atual_max)
+
+funcs , horas = funcionario_mes (funcmes)
+print(funcs)
