459607cdc88692fd18fd21a1da495d5ff37f1c51
Lucca Biasoli <lucca.bbiasoli@gmail.com>
2021-05-16 04:27:51 -0300

    1

---
+++ python_colas/asd.py
@@ -0,0 +1,8 @@
+student_grades = [8,4,1,3,2,6,9,7]
+student_grades.sort()
+print(student_grades[-1])
+
+student_grades = [9.1, 8.8, 10.0, 7.7, 6.8, 8.0, 10.0, 8.1, 10.0, 9.9]
+contar = student_grades.count(10)
+if contar >=3:
+    print('é sim')
---
+++ python_colas/sum_dir.py
@@ -0,0 +1,10 @@
+notas = [8,4,2]
+soma = sum(notas)  # serve para somar tudo
+tamanho = len(notas)  
+calculo = soma  / tamanho
+print(calculo)
+
+
+
+print(dir(notas))
+# print(dir(__builtins__))
